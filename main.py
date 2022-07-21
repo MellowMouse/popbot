216a634f8840f5914978a5a6d7942926a7c52331
MellowMouse <tiffer628@gmail.com>
2022-07-21 08:51:17 -0700

    made a list of emails to randomize them

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from jokes import joke_list
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import random
+
+#getting random joke
+num = random.randint(0,59)
+joke =joke_list[num]
 
 #setting driver to firefox and going to nate's site
-#PATH = "users/chrisaraujo/desktop/popbot/chromedriver.exe"
 PATH = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.get("https://apexsoccer.org/pods/")
@@ -17,12 +21,12 @@ elem.send_keys("Eric Jones")
 #enter a ridiculous email into the "Email field"
 elem = driver.find_element(By.ID, "form-field-email")
 elem.clear()
-elem.send_keys("picardlover69@hotmail.com")
+elem.send_keys("info@info125667")
 
 #enter the joke into the message field and submit
 elem = driver.find_element(By.ID, "form-field-message")
 elem.clear()
-elem.send_keys("Call 1 (888) 354-1369 for a chance to win a free cruise! Hurry before this offer run out!")
+elem.send_keys(joke)
 elem.send_keys(Keys.RETURN)
 
 #close my connection
