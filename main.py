92a624d7e8c9b78f26c917a6af2379082ed864bb
MellowMouse <tiffer628@gmail.com>
2022-07-21 10:08:12 -0700

    cleaned it up

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from emails import emails
 from jokes import joke_list
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,6 +8,10 @@ import random
 #getting random joke
 num = random.randint(0,59)
 joke =joke_list[num]
+
+#getting a random email
+rand_email = random.randint(0,9)
+email = emails[rand_email]
 
 #setting driver to firefox and going to nate's site
 PATH = "/usr/local/bin/chromedriver"
@@ -21,7 +26,7 @@ elem.send_keys("Eric Jones")
 #enter a ridiculous email into the "Email field"
 elem = driver.find_element(By.ID, "form-field-email")
 elem.clear()
-elem.send_keys("info@info125667")
+elem.send_keys(email)
 
 #enter the joke into the message field and submit
 elem = driver.find_element(By.ID, "form-field-message")
