cf339b41502acb2f63716e88f94609406520aa4b
MellowMouse <tiffer628@gmail.com>
2022-07-26 00:05:32 -0700

    edited email list

--- emails.py
+++ emails.py
@@ -1,2 +1,2 @@
 #list of personalized, fake emails to hopefully make nate laugh lol
-emails = ["picardlover69@hotmail.com", "bringbackburnnotice@yahoo.com","paymemybitties@gmail.com", "soccerislifecuzIneedmoney@aol.com", "tntknowsdrama@nudez.co.uk", "themoonisgorgeous@hotmail.com", "DeBruyne6969@lol.com", "thecroquemonsieurmonsieur@aol.com", "thebelgiansoggywaffle@gmail.com",  "letsgohikinginsteadofmakingout@hotmail.com"]
+emails = ["picardlover69@hotmail.com", "bringbackburnnotice@yahoo.com","paymemycummies@gmail.com", "soccerislifecuzIneedmoney@aol.com", "tntknowsdrama@nudez.co.uk", "themoonisgorgeous@hotmail.com", "DeBruyne6969@lol.com", "thecroquemonsieurmonsieur@aol.com", "thebelgiansoggywaffle@gmail.com",  "letsgohikinginsteadofmakingout@hotmail.com"]
